6f8212fa9ad15520a5d57bf6754dfb28eea3fe61
rawand1998 <rawandgaradh@gmail.com>
2022-11-07 12:32:16 +0200

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { AiOutlineMenu } from "react-icons/ai";
 function Navbar() {
   const [scrolled, setScrolled] = useState(false);
-
+  const menuRef = useRef(null);
+  const removeShadw = useRef(null);
+  const [toggle, setToggle] = useState(false);
   useEffect(() => {
     const onScroll = () => {
-      if (window.scrollY > 50) {
+      if (window.scrollY > 80) {
         setScrolled(true);
       } else {
         setScrolled(false);
@@ -18,6 +21,14 @@ function Navbar() {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
+  const toggleMenu = () => {
+    menuRef.current.classList.toggle("show__menu");
+    setToggle(true);
+  };
+  const hide_menu = () => {
+    console.log("hi");
+    removeShadw.current.classList.remove("show__menu");
+  };
   return (
     <div
       className={scrolled ? "navbar_container scrolled" : "navbar_container"}
@@ -27,20 +38,24 @@ function Navbar() {
           Rawand <span>Jaradh</span>
         </span>
       </div>
-      <div className="menu">
+      <div className= "menu" ref={menuRef}>
         <nav>
           <li>
-            <a href="">Home</a>
+            <a href="" onClick={hide_menu}>
+              {" "}
+              Home
+            </a>
           </li>
           <li>
-            <a href="">Skills</a>
+            <a href="" onClick={hide_menu}>
+              Skills
+            </a>
           </li>
           <li>
-            <a href="">Projects</a>
+            <a href="" onClick={hide_menu}>
+              Projects
+            </a>
           </li>
-          {/* <Link to="/">Home</Link>
-            <Link to="/">Skills</Link>
-            <Link to="/">Projects</Link> */}
         </nav>
         <div className="icons">
           <a href="">
@@ -52,6 +67,9 @@ function Navbar() {
         </div>
         <button className="contact_btn">Contact me</button>
       </div>
+      <span className="mobile__menu">
+        <AiOutlineMenu onClick={toggleMenu} className="AiOutlineMenu" />
+      </span>
     </div>
   );
 }
