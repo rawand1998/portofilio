ebb9cbfb77fbab7b5df70954d3ad3fe5bf76e9e1
rawand1998 <rawandgaradh@gmail.com>
2022-11-07 15:53:54 +0200

    contact form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Banner from "./components/Banner";
+import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects/>
+      <Contact/>
     </div>
   );
 }
