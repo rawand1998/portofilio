f872f3eae1a18a1fc8da0fc44defdeb080dfb343
rawand1998 <rawandgaradh@gmail.com>
2022-11-07 14:59:02 +0200

    skills section

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,3 +1,6 @@
-import './navbar.css'
-import './App.css'
-import './banner.css'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import "./navbar.css";
+import "./App.css";
+import "./banner.css";
+import "./skills.css";
